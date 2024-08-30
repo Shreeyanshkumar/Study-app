61dad5eb55529d8cf6591259cd331dc59197b0ca
Shreeyanshkumar <shreeyanshk.ug21.ec@nitp.ac.in>
2024-08-31 00:56:41 +0530

    setup basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home';
+import RootLayout from './components/Layout/Root';
+
+const router = createBrowserRouter([
+  {
+    id: 'root',
+    path: '/',
+    element: <RootLayout />,
+    children: [{
+      index: true,
+      path: '',
+      element: <Home />
+    }]
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
